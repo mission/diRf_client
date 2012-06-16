093a5dfdb7d68e60aa147250af3e46ad5141012f
stratum nine <strata@dropswitch.net>
2012-06-16 02:29:59 -0400

    Function declared twice.

--- code/sys/sys_win32.c
+++ code/sys/sys_win32.c
@@ -147,24 +147,6 @@ const char *Sys_TempPath( void )
 
 /*
 ================
-Sys_TempPath
-================
-*/
-const char *Sys_TempPath( void )
-{
-	static TCHAR path[ MAX_PATH ];
-	DWORD length;
-
-	length = GetTempPath( sizeof( path ), path );
-
-	if( length > sizeof( path ) || length == 0 )
-		return Sys_DefaultHomePath( );
-	else
-		return path;
-}
-
-/*
-================
 Sys_Milliseconds
 ================
 */
